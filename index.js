c9a62aa2997ff99dc0dfb8e528cf4f3d12edb012
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-06-28 14:47:29 +0300

    again

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 'use strict';
 require('dotenv').config();
-
 const server = require('./src/server');
-
-server.start(process.env.PORT || 8080)
+server.start(process.env.PORT || 8080);
--- src/server.js
+++ src/server.js
@@ -1,2 +1,28 @@
-'use strict'
-const packageName = require('packageName');
+'use strict';
+const express = require('express');
+const notFoundHandler = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.send('This server works.');
+});
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+
+
+
+module.exports = {
+  start: (port) => {
+    {
+      app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+      });
+    }
+  },
+};
