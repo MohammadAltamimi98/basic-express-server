e9999001d6c1b484d0d24221a45ae6bf24163efc
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-06-28 15:17:13 +0300

    second commit

--- src/server.js
+++ src/server.js
@@ -5,11 +5,28 @@ const errorHandler = require('./error-handlers/500');
 
 const app = express();
 
+const logger = require('./middleware/logger');
+
+app.use(logger);
 
 
 app.get('/', (req, res) => {
   res.send('This server works.');
 });
+
+
+app.get('/person', (req, res) => {
+  const output = {
+    name: req.query.name,
+  };
+  res.json(output);
+});
+
+
+app.get('/bad',(req,res)=>{
+  throw new Error('Error');
+});
+
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
